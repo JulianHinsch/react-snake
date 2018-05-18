7900189197e4f75b733353b9748ed433bddb913b
Julian Hinsch <jhinsch799@gmail.com>
2018-05-18 15:45:30 -0400

    fixed server code

--- server.js
+++ server.js
@@ -4,17 +4,7 @@ const path = require('path');
 
 const app = express();
 
-//serve static assets in production
-if (process.env.NODE_ENV !== 'development') {
-    app.use(express.static(__dirname+'/client/build'));
-}
-
-//general error handling middleware
-app.use((req,res,next) => {
-    const err = new Error('Internal Server Error');
-    err.status = 500;
-    next(err);
-});
+app.use(express.static(__dirname+'/client/build'));
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`listening on port:${port}`));
