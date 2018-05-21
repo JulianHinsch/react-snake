e7a6566d0c41df205c3ea7d34275dc098e9ae115
JulianHinsch <jhinsch799@gmail.com>
2018-05-21 10:36:33 -0400

    added App.js test stubs

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -12,4 +12,60 @@ describe('App', ()=>{
         ReactDOM.render(<App />, div);
         ReactDOM.unmountComponentAtNode(div);
     });
+
+    /**
+     * Test componentWillMount adds event listener
+     */
+
+     /**
+      * Test componentWillUnmount removes event listener
+      */
+
+    /**
+     * Test startNewGame sets an interval
+     */
+
+    /**
+     * Test startNewGame modifies state
+     */
+
+    /**
+     * Test handleKeyDown (enter)
+     */
+
+    /**
+     * Test handleKeyDown (left)
+     */
+
+    /**
+     * Test handleKeyDown (right)
+     */
+
+    /**
+     * Test handleKeyDown (up)
+     */
+
+    /**
+     * Test handleKeyDown (down)
+     */
+
+    /**
+     * Test getNextCell returns an object with keys x,y, with values 0-26
+     */
+
+    /**
+     * Test advanceSnake advances snake
+     */
+
+    /**
+     * Test moveFood moves food
+     */
+    
+    /**
+     * Test endGame modifies state
+     */
+
+    /**
+     * Test endGame clears interval
+     */
 })
